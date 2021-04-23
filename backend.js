7b8238c301e85f6b955572c3283684942d64615a
shikhars371 <shikhars371@gmail.com>
2021-04-23 23:53:25 +0530

    .

--- backend.js
+++ backend.js
@@ -1,33 +1,30 @@
 const { Client } = require('dhub')
 const DTree = require('dwebtree')
-const { kvPairs: dictionaryPairs } = require('websters-english-dictionary')
+const { kvPairs : dictionaryPairs } = require('websters-english-dictionary')
 
 start()
 
-async function start () {
+async function start() {
   const { basestore, replicate } = new Client()
   const store = basestore()
 
   // create storage for the dTree we're going to use to store the dictionary
   const base = store.get({ name: 'dictionary' })
 
-  // create the dTree
+  // create dTree
   const db = new DTree(base, { keyEncoding: 'utf-8', valueEncoding: 'utf-8' })
 
-  // store each definition in the dictionary, as key/value pairs using dTree's batch method
+  // store each definition in the dictionary as key/value pairs using dTree's batch method
   const batch = db.batch()
-  for (const { key,value } of dictionaryPairs()) {
+  for (const { key, value } of dictionaryPairs()) {
     await batch.put(key, value)
   }
 
-  // By flushing here, we insert all the key/values from the for-loop above into the dTree
   await batch.flush()
+  
+  // Print dTree key
+  console.log('The dTree key is:', db.key.toString('hex'))
 
-  // Print out the dTree's key to the console
-  console.log('The dTree key is:', base.key.toString('hex'))
-
-  // Now we announce the dTree on the dWeb and it can be queried from anywhere
-  while (true) {
-    await replicate(base)
-  }
+  // Now announce on dWeb
+  await replicate(db)
 }
